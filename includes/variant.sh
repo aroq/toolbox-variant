7e469396ad2390da22fe339e63107df653e76d8e
Alexander Tolstikov <atolstikov@adyax.com>
2020-02-03 17:37:27 +0300

    Initial commit

---
+++ includes/variant.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Set strict bash mode
+set -euo pipefail
+
+function _prepare_variant_env_vars {
+  local variant_env_file
+  variant_env_file="$(mktemp)"
+
+  export TOOLBOX_DOCKER_RUN_TOOL_ENV_FILE=${TOOLBOX_DOCKER_RUN_TOOL_ENV_FILE:-}
+  (env | grep ^VARIANT_) >> "${variant_env_file}"
+  TOOLBOX_DOCKER_RUN_TOOL_ENV_FILE="${TOOLBOX_DOCKER_RUN_TOOL_ENV_FILE} --env-file=${variant_env_file}"
+  _log DEBUG "${YELLOW}'VARIANT_*' variable list - ${variant_env_file}:${RESTORE}"
+  _log DEBUG "$(cat "${variant_env_file}")"
+
+  local variant_env_file="toolbox/.toolbox/.tmp/.variant.vars.env"
+  docker run --rm -i -t -w "$(pwd)" \
+    -v "$(pwd):$(pwd)" aroq/toolbox \
+    sh -c "yq r -j ${TOOL_PATH} | jq -r '. | recurse(.tasks[]?) | select(.bindParamsFromEnv == true) | .parameters | .[]? | .name' | uniq > ${variant_env_file}"
+
+  _log DEBUG "${YELLOW}Variable list generated from the variant command - ${variant_env_file}:${RESTORE}"
+  _log DEBUG "$(cat ${variant_env_file})"
+
+  if [[ -f "${variant_env_file}" ]]; then
+    TOOLBOX_DOCKER_RUN_TOOL_ENV_FILE="${TOOLBOX_DOCKER_RUN_TOOL_ENV_FILE} --env-file ${variant_env_file}"
+  fi
+}
