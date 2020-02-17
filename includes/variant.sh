6d3ecc10a7ad9719aea5a238cfa603318cbd1835
Alexander Tolstikov <atolstikov@adyax.com>
2020-02-17 15:49:03 +0300

    Update docker entrypoint

--- includes/variant.sh
+++ includes/variant.sh
@@ -7,6 +7,8 @@ function _prepare_variant_env_vars {
   local variant_env_file
   variant_env_file="$(mktemp)"
 
+  TOOLBOX_DOCKER_VARIANT_VARS_ENTRYPOINT=${TOOLBOX_DOCKER_VARIANT_VARS_ENTRYPOINT:-sh}
+
   export TOOLBOX_DOCKER_RUN_TOOL_ENV_FILE=${TOOLBOX_DOCKER_RUN_TOOL_ENV_FILE:-}
   (env | grep ^VARIANT_) >> "${variant_env_file}"
   TOOLBOX_DOCKER_RUN_TOOL_ENV_FILE="${TOOLBOX_DOCKER_RUN_TOOL_ENV_FILE} --env-file=${variant_env_file}"
@@ -15,8 +17,9 @@ function _prepare_variant_env_vars {
 
   local variant_env_file="toolbox/.toolbox/.tmp/.variant.vars.env"
   docker run --rm -i -t -w "$(pwd)" \
+    --entrypoint="${TOOLBOX_DOCKER_VARIANT_VARS_ENTRYPOINT}" \
     -v "$(pwd):$(pwd)" aroq/toolbox \
-    sh -c "yq r -j ${TOOL_PATH} | jq -r '. | recurse(.tasks[]?) | select(.bindParamsFromEnv == true) | .parameters | .[]? | .name' | uniq > ${variant_env_file}"
+    -c "yq r -j ${TOOL_PATH} | jq -r '. | recurse(.tasks[]?) | select(.bindParamsFromEnv == true) | .parameters | .[]? | .name' | uniq > ${variant_env_file}"
 
   _log DEBUG "${YELLOW}Variable list generated from the variant command - ${variant_env_file}:${RESTORE}"
   _log DEBUG "$(cat ${variant_env_file})"
@@ -25,3 +28,4 @@ function _prepare_variant_env_vars {
     TOOLBOX_DOCKER_RUN_TOOL_ENV_FILE="${TOOLBOX_DOCKER_RUN_TOOL_ENV_FILE} --env-file ${variant_env_file}"
   fi
 }
+
